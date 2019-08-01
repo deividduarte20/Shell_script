9e662e8603bb1cf298c9c0dad9f9db7b700cff13
deividdua <48442376+deividdua@users.noreply.github.com>
2019-08-01 15:01:01 -0300

    Script acessa site via terminal
    O script acessa site com o pacote lynx via terminal filtra com expressão regular e corta algumas informações com o cut e exibe o resultado da atualização stable do mikrotik na saída padrão

---
+++ mikrotik.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#
+# mikrotik.sh - Verifica versão do mikrotik no site
+#
+# Site:		https://deividduarte2012.blogspot.com
+# Autor:	Deivid Duarte
+# Manutenção: 	Deivid Duarte / Diego Henrique
+#
+# ----------------------------------------------------------------------------#
+# Este programa irá acessar o site no modo terminal filtrar a versão stable
+# do site e exibir na saída padrão (tela)
+#
+# Exemplos:
+# ./mikrotik.sh
+#
+#
+# ----------------------------------------------------------------------------#
+# Histórico:
+#  v1.0 01-01-2019 , Deivid:
+#	- Inicio do programa
+#
+# -------------------------------------EXECUÇÃO-------------------------------#
+SITE=$(lynx -source https://mikrotik.com/download | grep "^.th" | cut -c 22-27)
+
+echo $SITE
